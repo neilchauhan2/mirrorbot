dcb3df4f82fd0b4d31ed82391435b8ca99204bb6
Neil Chauhan <neilchauhan2@gmail.com>
2022-03-16 00:15:58 +0530

    Added ChatText component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import ChatText from "./components/ChatText";
 
 function App() {
   return (
     <div className="App">
       <h1>Chat App</h1>
+      <ChatText />
     </div>
   );
 }
---
+++ src/components/ChatText.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/ChatText.css";
+
+const ChatText = () => {
+  return (
+    <div className="chat-text-container user-response-text">
+      <p className="chat-text-body">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nam quam
+        sed alias ea sapiente sit maxime quaerat magni mollitia, voluptatibus
+        rerum nesciunt officia delectus molestiae perspiciatis ipsam id minus?
+      </p>
+    </div>
+  );
+};
+
+export default ChatText;
