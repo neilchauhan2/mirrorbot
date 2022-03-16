34895453ef7bb0e2b987e2c1bb385a4417637806
Neil Chauhan <neilchauhan2@gmail.com>
2022-03-17 00:41:42 +0530

    Added responsive styling.

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
       <span className="logo">
         <ion-icon name="flash"></ion-icon>
       </span>
-      <h1 className="brand">MirrBot</h1>
+      <h1 className="brand">MirrorBot</h1>
     </div>
   );
 };
